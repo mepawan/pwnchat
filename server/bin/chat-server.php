29fb122a2bc63fa22536e8c31a625cf71fb0f03f
Nirmala <meshivani00@gmail.com>
2015-09-24 13:46:20 +0530

    initail commit

--- server/bin/chat-server.php
+++ server/bin/chat-server.php
@@ -39,7 +39,7 @@ function createServer(){
         $wsServer = new OriginCheck($wsServer, $config['allowedOrigins']);
     }
 
-    $server = MgRTC\Server::factory(new HttpServer($wsServer), $config['port'],'127.0.0.1');
+    $server = MgRTC\Server::factory(new HttpServer($wsServer), $config['port'],'104.197.28.131');
 }
 
 /**
